9ee4f2b2f9b9876839d08732c0b6350c80f40a3f
danylo8 <ozai.avatar@gmail.com>
2024-11-27 11:49:22 -0500

    Got to creating the method for total sum

---
+++ Algorithms.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Algorithms {
+    private static File f;
+    private static Scanner s;
+    public static void main(String[] args) throws FileNotFoundException {
+        f = new File("Numbers.txt");
+        int odds = odds();
+        int evens = evens();
+        int twoDigits=twoDigits();
+        int greaterThan500=greaterThan500();
+        System.out.println(odds);
+        System.out.println(evens);
+        System.out.println(twoDigits); // check why s.nextInt() has to be replaced with n
+        System.out.println(greaterThan500);
+        s.close();
+    }
+
+    public static int odds() throws FileNotFoundException{
+        s = new Scanner(f);
+        int odds = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() % 2 != 0)
+                odds++;
+        }
+        return odds;
+    }
+
+    public static int evens() throws FileNotFoundException{
+        s = new Scanner(f);
+        int evens = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() % 2 == 0)
+                evens++;
+        }
+        return evens;
+    }
+
+    public static int twoDigits() throws FileNotFoundException{
+        s = new Scanner(f);
+        int twoDigits = 0;
+
+        while (s.hasNext()) {
+            int n = s.nextInt();
+            if ( n>=10 && n <=99)
+                twoDigits++;
+        }
+        return twoDigits;
+    }
+
+    public static int greaterThan500() throws FileNotFoundException{
+        s = new Scanner(f);
+        int greaterThan500 = 0;
+
+        while (s.hasNext()) {
+            int n = s.nextInt();
+            if ( n>500 )
+                greaterThan500++;
+        }
+        return greaterThan500;
+
+
+    }
+
+    public static int totalsum() throws FileNotFoundException{
+        s = new Scanner(f);
+        int totalsum = 0;
+
+        while (s.hasNext()) {
+            int n = s.nextInt();
+            totalsum+=n;
+        }
+        return totalsum;
+    }
